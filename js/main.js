894450a354dc38d64918d247966c6a76b3df1a75
Yakovishchuk Maxym <yakovishchuk2004@gmail.com>
2022-08-07 15:52:14 +0300

    main

---
+++ js/main.js
@@ -0,0 +1,55 @@
+$(function(){
+    $('.header-slider').slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="images/prep.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
+        fade: true,
+        responsive: [{
+          breakpoint: 361,
+          settings: {
+            dots: false,
+            arrows: false,
+            autoplay: true,
+            autoplaySpeed: 2000,
+          }
+        }]
+
+    });
+
+    $('.product__name').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        focusOnSelect: true,
+        asNavFor: '.product__content',
+        vertical: true,
+        prevArrow: '<button type="button" class="product-prev"><img src="images/product-prep.svg" alt=""></button>',
+        nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+        responsive: [{
+          breakpoint: 891,
+          settings: {
+            arrows: false,
+            vertical: false,
+            slidesToShow: 3,
+            dots: true,
+          },
+          breakpoint: 461,
+          settings: {
+            slidesToShow: 1,
+            vertical: false,
+            arrows: false,
+            dots: true,
+          },
+        }]
+      });
+      $('.product__content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.product__name',
+        fade: true,
+        arrows: false,
+      });
+
+      $('.menu__btn').on('click', function(){
+        $('.menu__list').toggleClass('menu__list--active');
+      });
+});
